2add339515de46f64ee31b5722ea3a6e43d193b4
Jose Otniel <joseotniel2010@gmail.com>
2021-06-28 10:54:02 -0400

    Made department table: my department table is working

--- ClientApp/src/components/Department.js
+++ ClientApp/src/components/Department.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import { Table, Button, ButtonGroup} from 'reactstrap';
-import AddDeptModal from './PopUp Modals/AddDeptModal'
+import { Table, Button, ButtonToolbar, ButtonGroup} from 'reactstrap';
+import AddDeptModal from './PopUp Modals/AddDeptModal';
+import EditDeptModal from './PopUp Modals/EditDeptModal';
 
 export default class Department extends React.Component {
     constructor(props) {
@@ -9,13 +10,39 @@ export default class Department extends React.Component {
 
     state = {
         depts: [],
-        modelOpened: false
+        modalAddOpened: false,
+        modalEditOpened: false,
+        targetToEdit: {}
     }
 
-    toggleModel = () => this.setState(state => ({
-        depts: [...state.depts],
-        modelOpened: !state.modelOpened
+    toggleAddModal = () => this.setState(state => ({
+        modalAddOpened: !state.modalAddOpened
     }));
+
+    toggleEditModal = () => this.setState(state => ({
+        modalEditOpened: !state.modalEditOpened
+    }));
+
+    enterEditMode = (model) => {
+        this.setState({targetToEdit: model});
+        this.toggleEditModal();
+    }
+
+    enterDeleteMode = (model) => {
+        if(window.confirm("Are you sure you want to delete this department? (This action can't be undone)")) {
+            this.deleteDepartment(model.DepartmentId);
+            this.refreshDepartmentsList();
+        }
+    }
+    
+    deleteDepartment = (targetId) => {
+        fetch(process.env.REACT_APP_API + "department/" + `${targetId}`, {
+            method: 'DELETE',
+            headers: {'Content-type': 'application/json'}
+        })
+            .then(response => response.json())
+            .catch(error => alert(error));
+    }
 
     refreshDepartmentsList = () => {
         fetch(process.env.REACT_APP_API + "department")
@@ -60,15 +87,30 @@ export default class Department extends React.Component {
                                     <tr key={idx}>
                                         <td>{department.DepartmentId}</td>
                                         <td>{department.DepartmentName}</td>
-                                        <td>Edit | Delete</td>
+                                       
+                                        {/* Department actions */}
+                                        <td>
+
+                                            <ButtonToolbar>
+                                                <ButtonGroup size="sm">
+                                                    <Button color="info" outline onClick={this.enterEditMode.bind(this, department)}>Edit</Button>
+                                                    <Button color="danger" outline onClick={this.enterDeleteMode.bind(this, department)}>Delete</Button>
+                                                </ButtonGroup>
+                                            </ButtonToolbar>
+
+                                        </td>
+                                   
                                     </tr>
                                 ))
                         }
                     </tbody>
                 </Table>
                 
-                <Button className="mb-5" color="primary" onClick={this.toggleModel} size="lg">Add a new department</Button> 
-                <AddDeptModal isOpen={this.state.modelOpened} toggle={this.toggleModel} />
+                <Button className="mb-5" color="primary" onClick={this.toggleAddModal} size="lg">Add a new department</Button> 
+                
+                {/* Pop up Add modal */}
+                <AddDeptModal isOpen={this.state.modalAddOpened} toggle={this.toggleAddModal} />
+                <EditDeptModal isOpen={this.state.modalEditOpened} toggle={this.toggleEditModal} model={this.state.targetToEdit} />
             </div>
         )
     }
